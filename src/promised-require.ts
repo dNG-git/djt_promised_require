fa08410b552cb7328c03d45132163e688c63561c
NotTheEvilOne <git-NTEO@vplace.de>
2018-07-27 08:37:28 +0200

    Enhancement: Add support for dynamic imports of browser modules

--- src/promised-require.ts
+++ src/promised-require.ts
@@ -19,6 +19,11 @@
  */
 // tslint:disable-next-line:no-any
 declare function require(arg1: any, ...args: any[]): any;
+/**
+ * Override for browser 'self' context.
+ */
+// tslint:disable-next-line:no-any
+declare var self: any;
 
 /**
  * "PromisedRequire" provides a "require()" implementation for CommonJS as
@@ -33,13 +38,22 @@ declare function require(arg1: any, ...args: any[]): any;
  */
 export default class PromisedRequire {
     /**
+     * Browser dynamic "import()" implementation
+     */
+    protected static readonly BROWSER_MODULE_IMPORT = 1;
+    /**
      * AMD "require()" implementation
      */
-    protected static readonly BROWSER_REQUIRE = 1;
+    protected static readonly BROWSER_REQUIRE = 2;
     /**
      * node (CommonJS) "require()" implementation
      */
-    protected static readonly NODE_REQUIRE = 2;
+    protected static readonly NODE_REQUIRE = 3;
+    /**
+     * Function encapsuled import call
+     */
+    // tslint:disable-next-line:ban-types
+    private static dynamicImport: Function;
 
     /**
      * Flag indicating that the tag name has been registered
@@ -55,54 +69,96 @@ export default class PromisedRequire {
      * @since  v1.0.0
      */
     public static async require(...modules: string[]) {
-        if (this.requireImplementation === undefined) {
-            if (typeof require != 'undefined') {
-                this.requireImplementation = (
-                    typeof __filename != 'undefined' ?
-                    this.NODE_REQUIRE : this.BROWSER_REQUIRE
-                );
-            }
-        }
+        let promise;
 
-        const promise = new Promise((resolve, reject) => {
-            // tslint:disable-next-line:no-any
-            const moduleList: any = { };
-
-            switch (this.requireImplementation) {
-                case this.BROWSER_REQUIRE:
-                    require(
-                        modules,
-                        (...modulesLoaded: object[]) => {
-                            modulesLoaded.forEach((moduleLoaded: object, i: number) => {
-                                moduleList[modules[i]] = moduleLoaded;
-                            });
-
-                            resolve(moduleList);
-                        },
-                        (err: Error) => {
-                            reject(err);
-                        }
+        if (
+            this.requireImplementation === this.BROWSER_MODULE_IMPORT
+            || (
+                this.requireImplementation === undefined
+                && (typeof self == 'undefined' || self.document.location.protocol.toLowerCase() != 'file:')
+               )
+           ) {
+            try {
+                if (!this.dynamicImport) {
+                    this.dynamicImport = new Function(
+                        'module',
+                        'if (module.match(/^\\w/)) { module = \'./\' + module + \'.js\'; } return import(module);'
                     );
+                }
 
-                    break;
-                case this.NODE_REQUIRE:
-                    try {
-                        for (const moduleSpec of modules) {
-                            const moduleLoaded = require(moduleSpec);
+                promise = Promise.all(modules.map((module) => this.dynamicImport(module)));
 
-                            moduleList[moduleSpec] = (moduleLoaded.default ? moduleLoaded.default : moduleLoaded);
-                        }
+                // tslint:disable-next-line:no-any
+                promise.then((modulesLoaded: any[]) => {
+                    let i = 0;
+                    // tslint:disable-next-line:no-any
+                    const moduleList: any = { };
 
-                        resolve(moduleList);
-                    } catch (handledException) {
-                        reject(handledException);
+                    for (const moduleSpec of modules) {
+                        moduleList[moduleSpec] = (
+                            modulesLoaded[i].default ? modulesLoaded[i].default : modulesLoaded[i]
+                        );
+
+                        i++;
                     }
 
-                    break;
-                default:
-                    reject(new Error('No require implementation available'));
-            }
-        });
+                    return moduleList;
+                });
+
+                if (this.requireImplementation === undefined) {
+                    this.requireImplementation = this.BROWSER_MODULE_IMPORT;
+                }
+            } catch (unhandledException) { /* Fallback */ }
+        }
+
+        if (this.requireImplementation === undefined && typeof require != 'undefined') {
+            this.requireImplementation = (
+                typeof __filename != 'undefined' ?
+                this.NODE_REQUIRE : this.BROWSER_REQUIRE
+            );
+        }
+
+        if (!promise) {
+            promise = new Promise((resolve, reject) => {
+                // tslint:disable-next-line:no-any
+                const moduleList: any = { };
+
+                switch (this.requireImplementation) {
+                    case this.BROWSER_REQUIRE:
+                        require(
+                            modules,
+                            (...modulesLoaded: object[]) => {
+                                modulesLoaded.forEach((moduleLoaded: object, i: number) => {
+                                    moduleList[modules[i]] = moduleLoaded;
+                                });
+
+                                resolve(moduleList);
+                            },
+                            (err: Error) => {
+                                reject(err);
+                            }
+                        );
+
+                        break;
+                    case this.NODE_REQUIRE:
+                        try {
+                            for (const moduleSpec of modules) {
+                                const moduleLoaded = require(moduleSpec);
+
+                                moduleList[moduleSpec] = (moduleLoaded.default ? moduleLoaded.default : moduleLoaded);
+                            }
+
+                            resolve(moduleList);
+                        } catch (handledException) {
+                            reject(handledException);
+                        }
+
+                        break;
+                    default:
+                        reject(new Error('No require implementation available'));
+                }
+            });
+        }
 
         return promise;
     }
